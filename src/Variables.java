5207307d5abdec16cbc1e6dc400c09b88aaa75ed
100173778 <100173778@lubbockisd.net>
2023-09-12 09:50:32 -0500

    this is my variables project

---
+++ src/Variables.java
@@ -0,0 +1,30 @@
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+//define 1 variable of each of the
+//following data types
+//byte short int long
+//float double
+//char boolean String
+//integer variables
+        byte byteOne = 127;
+        System.out.println("/////////////////////////////////");
+        System.out.println("*Some Person\t\t\t07/18/02*");
+        System.out.println("* \t\t\t\t\t\t\t\t*");
+        System.out.println("*\t\t integer types \t\t\t*");
+        System.out.println("* \t\t\t\t\t\t\t\t*");
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t\t*");
+        System.out.println("*16 bit - shortOne = -32123\t\t*");
+        System.out.println("*32 bit - intOne = 90877 \t\t*");
+        System.out.println("*32 bit - intOne = 90877 \t\t*");
+        System.out.println("*\t\t  real types\t\t\t*");
+        System.out.println("*32 bit - floatOne = 38.5678\t*");
+        System.out.println("*64 bit - doubleOne = 923.234\t*");
+        System.out.println("*\tother integer types\t\t\t*");
+        System.out.println("*booleanOne = true\t\t\t\t*");
+        System.out.println("*stringOne = hello world\t\t*");
+        System.out.println("/////////////////////////////////");
+
+    }
+}
